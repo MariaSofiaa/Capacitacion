188f63b5a5991092792a2944876960c21d5ff7c3
Sofia Landavazo Aguilar <slandavazo@TUFESA.local>
2021-12-08 16:51:02 -0700

    03:04:

--- src/app/pages/checkout/details/details.component.ts
+++ src/app/pages/checkout/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from 'src/app/shared/services/shopping-cart.service';
 
 @Component({
   selector: 'app-details',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  total$ = this.shoppingCartSvc.totalAction$;
+  cart$ = this.shoppingCartSvc.cartAction$;
 
-  constructor() { }
-  
+
+  constructor(private shoppingCartSvc: ShoppingCartService) { }
+
   ngOnInit(): void {
   }
 
